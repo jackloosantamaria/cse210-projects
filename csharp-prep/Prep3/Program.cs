039b7bf7e0e93253c6d68d143ee58cff8d6211dc
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-09-17 17:02:07 -0500

    added code, delete by mistake

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Core Requirement #1
+        Console.WriteLine("What is the magic number? ");
+        String magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber);
+        Console.WriteLine("What is your guess? ");
+        string gnumber = Console.ReadLine();
+        int guessNumber = int.Parse(gnumber);
+        if (number > guessNumber){
+            Console.WriteLine("Higher");
+        }
+        else if (number < guessNumber){
+            Console.WriteLine("Lower");
+        }
+        else{
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
