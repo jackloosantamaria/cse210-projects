b071b19b70745c5c6764ad7889656484b876a17d
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-09-17 20:05:36 -0500

    complete task #5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        String Username = PromptUserName();
+        int Numbers = PromptUserNumber();
+        int newSqNumb = SquareNumber(Numbers);
+        DisplayResult(Username, newSqNumb);
     }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static String PromptUserName(){
+        Console.Write("Please enter your name: ");
+        String name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+        
+    }
+
+    static int SquareNumber (int squareNum){
+        int newNum = squareNum * squareNum;
+        return newNum;
+    }
+
+    static void DisplayResult(String userName, int finalNumber){
+        Console.Write($"{userName}, the square of your number is {finalNumber}");
+        
+    }
+
 }
