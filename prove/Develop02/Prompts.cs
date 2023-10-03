c26fbfd5f0af296e46b54088ffd7ae55418bcdad
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-10-03 13:38:09 -0500

    added spaces and remove method

--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -1,10 +1,10 @@
 public class Prompts{
 
     public static string[] _options = {
-        "Who was the most interesting person I interacted with today?",
-        "What was the best part of my day?",
-        "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "Who was the most interesting person I interacted with today? ",
+        "What was the best part of my day? ",
+        "What was the strongest emotion I felt today? ",
+        "If I had one thing I could do over today, what would it be? "
     };
 
     public List<string> _Prompts = new List<string>(_options);
@@ -21,11 +21,6 @@ public class Prompts{
         return Prompts;
     }
 
-    public void Display(){
-        var random = new Random();
-        int number = random.Next(_Prompts.Count);
-        string Prompts = _Prompts[number];
-        Console.WriteLine($"\n{_Prompts}");
-    }
+
 
 }
