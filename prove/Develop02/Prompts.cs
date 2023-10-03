2d02f34392a873f163b25102dbe0e7c0fa8ea604
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-10-03 17:41:59 -0500

    added new line to prompts

--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -1,11 +1,15 @@
+using System.Reflection.Metadata.Ecma335;
+
 public class Prompts{
 
     public static string[] _options = {
         "Who was the most interesting person I interacted with today? ",
         "What was the best part of my day? ",
         "What was the strongest emotion I felt today? ",
-        "If I had one thing I could do over today, what would it be? "
+        "If I had one thing I could do over today, what would it be? ",
+        "What's on my mind today? "
     };
+
 
     public List<string> _prompts = new List<string>(_options);
 
