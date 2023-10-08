7f4aecb01a0df7049d47163d800046557b1e5c6c
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-10-08 17:32:21 -0500

    practice activity week 5 including class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+
+        Fraction p1 = new Fraction();
+        Console.WriteLine(p1.GetFractionString());
+        Console.WriteLine(p1.GetDecimalValue());
+
+        Fraction p2 = new Fraction(5);
+        Console.WriteLine(p2.GetFractionString());
+        Console.WriteLine(p2.GetDecimalValue());
+
+        Fraction p3 = new Fraction(3,4);
+        Console.WriteLine(p3.GetFractionString());
+        Console.WriteLine(p3.GetDecimalValue());
+
+        Fraction p4 = new Fraction(1,3);
+        Console.WriteLine(p4.GetFractionString());
+        Console.WriteLine(p4.GetDecimalValue());
+
     }
 }
