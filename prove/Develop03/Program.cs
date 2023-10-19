9e771bba6ceadb3d83fcd8b16ca7505687bafbbb
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-10-19 12:48:35 -0500

    added main class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        //Console.WriteLine("Hello Develop03 World!");
+        Console.Clear();
+        Reference reference = new Reference();
+        reference.SubmitFile();
+        Scripture scripture = new Scripture();
+        scripture.LoadVerses();
+        Words words = new Words();
+        reference.DisplayReference();
+        string escrit = scripture.GetAleatoryVerse();
+        string ref1 = reference.DisplayPhrase(scripture);
+        words.GetRenderedText(scripture);
+        while(words._hide.Count < words._answer.Length){
+            words.Show(ref1);
+            words.GetUserNumber();
+        }
+        words.Show(ref1);
     }
 }
