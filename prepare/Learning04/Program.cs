a124666a7868fb0d5d423ae0b27e68cab1f6cba3
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-10-23 17:12:35 -0500

    Main

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,15 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Learning04 World!");
+            Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine(assignment1.GetSummary());
+            MathAssignment ma2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            Console.WriteLine(ma2.GetSummary());
+            Console.WriteLine(ma2.GetHomeworkList());
+            WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        }
     }
-}
