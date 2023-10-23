11187a87a48091a6908d5e0caeab00c70a4f9c0f
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-10-23 17:31:12 -0500

    updated line

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,9 +7,13 @@ using System;
             //Console.WriteLine("Hello Learning04 World!");
             Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
             Console.WriteLine(assignment1.GetSummary());
+            Console.WriteLine("");
             MathAssignment ma2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
             Console.WriteLine(ma2.GetSummary());
             Console.WriteLine(ma2.GetHomeworkList());
+            Console.WriteLine("");
             WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(a3.GetSummary());
+            Console.WriteLine(a3.GetWritingInformation());
         }
     }
