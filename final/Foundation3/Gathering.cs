56fd3674e687fc2a1c518693669a5abee3aa0d97
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-12-05 16:29:36 -0500

    updating child class foundation 3

--- final/Foundation3/Gathering.cs
+++ final/Foundation3/Gathering.cs
@@ -5,31 +5,17 @@ public class Gathering:Event{
         _climate = climate;
     }
 
-    public string GetWeather(){
-        return _climate;
-    }
-
-    public string CreateReport(){
-        string word = "The weather will be ";
-        return word + _climate;
-    }
-
-    public string EventType(){
-        string info2 = "Outdoor Gathering";
-        return info2;
-    }
-
     public string GatheringDetail(){
         string data = "";
-        string weatherInfo = CreateReport();
+        string weatherInfo = "The weather will be ";
         data += CreateEvent();
-        data += "Weather Statement: " + weatherInfo;
+        data += "Weather Statement: " + weatherInfo + " " + _climate;
         return data;
     }
 
     public string CreateEventGathering(){
         string shortGathering = "";
-        string type = EventType();
+        string type = "Outdoor Gathering";
         shortGathering += "Event Type: " + type;
         shortGathering += CreateShortEvent();
         return shortGathering;
