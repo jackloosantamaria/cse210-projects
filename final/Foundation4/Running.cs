7fe246b298dd62f29b59f6c05265675b00e92818
jackloosantamaria <jackloosantamaria@hotmail.com>
2023-12-04 20:26:13 -0500

    child class foundation 4

---
+++ final/Foundation4/Running.cs
@@ -0,0 +1,26 @@
+public class Running : Exercise{
+    private double _distance;
+
+    public Running(string date, int time, double distance):base(date, time){
+        _name = "Running";
+        _distance = distance;
+    }
+
+    public override double Distance()
+    {
+        return _distance;   
+    }
+
+    public override double Speed()
+    {
+        double velocity = _distance/_time * 60;
+        return velocity;
+    }
+
+    public override double Pace()
+    {
+        double pace = _time/_distance;
+        double pace2 = Math.Round(pace, 2);
+        return pace2;
+    }
+}
